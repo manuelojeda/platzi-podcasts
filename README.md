186e41e97e6ddec9fbf82fbbb95cb7e1273ad1f5
Manuel Ojeda <manuelojedasistemas@gmail.com>
2019-11-18 00:56:49 -0700

    README.md added

---
+++ README.md
@@ -0,0 +1,18 @@
+# App de Podcasts de Platzi
+
+App de Podcasts integrada con la API de AudioBoom para aprender Next.js
+
+![Captura de la App](./.readme-static/captura.png)
+
+## ¿Cómo funciona?
+
+Requiere NodeJS LTS (12)
+
+* `npm install` para instalar dependencias
+* `npm run dev` para correr la aplicación en modo desarrollo
+* `npm run start` para correr la aplicación en modo producción sin construir una build final
+* `npm run build` para correr crear la build para producción
+
+# Licencia
+
+MIT
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import NProgress from 'nprogress'
 import { Helmet } from 'react-helmet'
+import Router from 'next/router'
+
+Router.events.on('routeChangeStart', () => NProgress.start() );
+Router.events.on('routeChangeComplete', () => NProgress.done() );
+Router.events.on('routeChangeError', () => NProgress.done() );
 
 const Layout = ({ title, SeoTitle, children }) => {
   return (
@@ -19,7 +25,6 @@ const Layout = ({ title, SeoTitle, children }) => {
       {
         children
       }
-
       <style jsx>{`
         header {
           color: #fff;
@@ -34,6 +39,82 @@ const Layout = ({ title, SeoTitle, children }) => {
           width: 100%;
           display: block;
         }
+      `}</style>
+      <style jsx global>{`
+        /* Make clicks pass-through */
+        #nprogress {
+          pointer-events: none;
+        }
+
+        #nprogress .bar {
+          background: #fff;
+
+          position: fixed;
+          z-index: 1031;
+          top: 0;
+          left: 0;
+
+          width: 100%;
+          height: 2px;
+        }
+
+        /* Fancy blur effect */
+        #nprogress .peg {
+          display: block;
+          position: absolute;
+          right: 0px;
+          width: 100px;
+          height: 100%;
+          box-shadow: 0 0 10px #fff, 0 0 5px #fff;
+          opacity: 1.0;
+
+          -webkit-transform: rotate(3deg) translate(0px, -4px);
+              -ms-transform: rotate(3deg) translate(0px, -4px);
+                  transform: rotate(3deg) translate(0px, -4px);
+        }
+
+        /* Remove these to get rid of the spinner */
+        #nprogress .spinner {
+          display: block;
+          position: fixed;
+          z-index: 1031;
+          top: 15px;
+          right: 15px;
+        }
+
+        #nprogress .spinner-icon {
+          width: 18px;
+          height: 18px;
+          box-sizing: border-box;
+
+          border: solid 2px transparent;
+          border-top-color: #fff;
+          border-left-color: #fff;
+          border-radius: 50%;
+
+          -webkit-animation: nprogress-spinner 400ms linear infinite;
+                  animation: nprogress-spinner 400ms linear infinite;
+        }
+
+        .nprogress-custom-parent {
+          overflow: hidden;
+          position: relative;
+        }
+
+        .nprogress-custom-parent #nprogress .spinner,
+        .nprogress-custom-parent #nprogress .bar {
+          position: absolute;
+        }
+
+        @-webkit-keyframes nprogress-spinner {
+          0%   { -webkit-transform: rotate(0deg); }
+          100% { -webkit-transform: rotate(360deg); }
+        }
+        @keyframes nprogress-spinner {
+          0%   { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+
       `}</style>
     </>
   )
